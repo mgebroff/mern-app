59f05aa346b510217161d5b1cd1bb8a6c823e3d9
mgebrov <137835446+mgebrov@users.noreply.github.com>
2023-08-11 13:54:53 -0700

    fix front-end

--- server.js
+++ server.js
@@ -43,14 +43,16 @@ app.get("/api/v1/test", (req, res) => {
   res.json({ msg: "test rout" });
 });
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+//app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  // res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 /******************* ERRORS ****************************************************/
